ff70e83bc3b612c4a842113c3960dd3ec42f97f6
onticinc <onticinc@gmail.com>
2021-12-03 10:36:55 -0700

    added models

---
+++ migrations/20211203173619-create-item.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Items', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      itemTitle: {
+        type: Sequelize.STRING
+      },
+      itemDescription: {
+        type: Sequelize.STRING
+      },
+      itemCost: {
+        type: Sequelize.INTEGER
+      },
+      itemQuantity: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Items');
+  }
+};
