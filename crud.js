215046030135c4ceae0df78b122ff9c3b903dbf6
onticinc <onticinc@gmail.com>
2021-12-03 11:10:33 -0700

    finished crud

--- crud.js
+++ crud.js
@@ -1,10 +1,22 @@
-const { User } = require(".models");
+const { User, item } = require(".models");
 
 User.create({
     firstName: 'Eric',
     lastName: 'Nielsen',
     age: 56,
     email: 'email@email.com'
+})
+
+    .catch(function (error) {
+        console.log(error);
+    });
+
+
+Item.create({
+    fitemTitle: 'X32 Mixer',
+    itemDescription: '32 Channel Audio Mixer',
+    itmCost: 1250,
+    itemQuantity: '15'
 })
 
     .catch(function (error) {
